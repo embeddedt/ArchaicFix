3de3e453d77239d3d0ccf153e54b919368acf39a
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-05-02 17:22:02 -0400

    Remove initial chunk load patch

--- src/main/java/org/embeddedt/archaicfix/ArchaicFix.java
+++ src/main/java/org/embeddedt/archaicfix/ArchaicFix.java
@@ -1,9 +1,8 @@
 package org.embeddedt.archaicfix;
 
-import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.event.*;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = ArchaicFix.MODID, version = ArchaicFix.VERSION)
 public class ArchaicFix
@@ -12,8 +11,8 @@ public class ArchaicFix
     public static final String VERSION = "1.0";
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
+    public void preinit(FMLPreInitializationEvent event)
     {
-
     }
+
 }
