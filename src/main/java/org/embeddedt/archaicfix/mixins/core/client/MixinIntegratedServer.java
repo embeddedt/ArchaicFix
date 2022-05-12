d75c89d69a4c531dc48b9c815b84f3e9ef879bc3
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-05-12 12:34:06 -0400

    Force integrated server to be at a minimum of 10 chunks

---
+++ src/main/java/org/embeddedt/archaicfix/mixins/core/client/MixinIntegratedServer.java
@@ -0,0 +1,16 @@
+package org.embeddedt.archaicfix.mixins.core.client;
+
+import net.minecraft.client.settings.GameSettings;
+import net.minecraft.server.integrated.IntegratedServer;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(IntegratedServer.class)
+public class MixinIntegratedServer {
+    @Redirect(method = "tick", at = @At(value = "FIELD", opcode = Opcodes.GETFIELD, target = "Lnet/minecraft/client/settings/GameSettings;renderDistanceChunks:I"))
+    private int getRealRenderDistance(GameSettings settings) {
+        return Math.max(settings.renderDistanceChunks, 10);
+    }
+}
