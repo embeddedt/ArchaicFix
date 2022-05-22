ff22e3f8d3f20f0592f9b26cdc2ce7153abb06ee
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-05-22 14:55:06 -0400

    Renderer tweaks

--- src/main/java/org/embeddedt/archaicfix/mixins/core/client/MixinWorldRenderer.java
+++ src/main/java/org/embeddedt/archaicfix/mixins/core/client/MixinWorldRenderer.java
@@ -1,22 +1,42 @@
 package org.embeddedt.archaicfix.mixins.core.client;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraft.world.ChunkCache;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /* MC-129 */
 @Mixin(WorldRenderer.class)
-public class MixinWorldRenderer {
+public abstract class MixinWorldRenderer {
     @Shadow private boolean isInitialized;
     @Shadow public boolean needsUpdate;
 
-    @Inject(method = "skipAllRenderPasses", at = @At("RETURN"), cancellable = true)
-    public void dontSkipRenderIfNeedsUpdate(CallbackInfoReturnable<Boolean> cir) {
-        if(this.isInitialized && this.needsUpdate) {
-            cir.setReturnValue(false);
+    @Shadow public abstract float distanceToEntitySquared(Entity p_78912_1_);
+
+    @Shadow public boolean[] skipRenderPass;
+
+    @Inject(method = "markDirty", at = @At("TAIL"))
+    private void forceRender(CallbackInfo ci) {
+        for(int i = 0; i < this.skipRenderPass.length; i++) {
+            this.skipRenderPass[i] = false;
         }
+    }
+
+    @Redirect(method = "updateRenderer", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/ChunkCache;extendedLevelsInChunkCache()Z"))
+    private boolean checkEmpty(ChunkCache cache) {
+        if(cache.extendedLevelsInChunkCache())
+            return true;
+        if(Minecraft.getMinecraft().renderViewEntity == null)
+            return false;
+        float distance = this.distanceToEntitySquared(Minecraft.getMinecraft().renderViewEntity);
+        int renderDistanceBlocks = Minecraft.getMinecraft().gameSettings.renderDistanceChunks * 16;
+        return distance > (renderDistanceBlocks * renderDistanceBlocks);
     }
 }
