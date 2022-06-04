f028823a2c555ec7c2d8ba06bb53d3e7e40d010c
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-06-04 12:03:23 -0400

    Fix broken fence AO

---
+++ src/main/java/org/embeddedt/archaicfix/mixins/core/client/MixinBlockFence.java
@@ -0,0 +1,21 @@
+package org.embeddedt.archaicfix.mixins.core.client;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockFence;
+import net.minecraft.block.material.Material;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BlockFence.class)
+public abstract class MixinBlockFence extends Block {
+    protected MixinBlockFence(Material p_i45394_1_) {
+        super(p_i45394_1_);
+    }
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void makeTransparent(String s, Material m, CallbackInfo ci) {
+        this.canBlockGrass = true;
+    }
+}
