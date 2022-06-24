db0181bc3947d0359817197cbf28b77af45adffd
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-06-23 21:28:48 -0400

    Remove step height fix
    I contributed a better version of this to EFR.

--- src/main/java/org/embeddedt/archaicfix/mixins/core/common/MixinEntity.java
+++ src/main/java/org/embeddedt/archaicfix/mixins/core/common/MixinEntity.java
@@ -1,34 +1,15 @@
 package org.embeddedt.archaicfix.mixins.core.common;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.crash.CrashReport;
-import net.minecraft.crash.CrashReportCategory;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
-import net.minecraft.util.AxisAlignedBB;
-import net.minecraft.util.MathHelper;
-import net.minecraft.util.ReportedException;
-import net.minecraft.world.World;
-import org.objectweb.asm.Opcodes;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-
-import java.util.List;
 
 @Mixin(Entity.class)
 public abstract class MixinEntity {
     @Shadow public float fallDistance;
-
-    @Shadow @Final public AxisAlignedBB boundingBox;
 
     /**
      * @reason Fixes a vanilla bug where the entity's fall distance is not updated before invoking the
@@ -38,24 +19,5 @@ public abstract class MixinEntity {
     @Inject(method = "updateFallState", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;fall(F)V"))
     private void beforeOnFallenUpon(double distanceFallenThisTick, boolean isOnGround, CallbackInfo ci) {
         if (distanceFallenThisTick < 0) fallDistance -= distanceFallenThisTick;
-    }
-
-    private AxisAlignedBB arch$savedBB;
-    @Inject(method = "moveEntity",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getCollidingBoundingBoxes(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/AxisAlignedBB;)Ljava/util/List;", ordinal = 0, shift = At.Shift.AFTER),
-            slice = @Slice(from = @At(value = "FIELD", opcode = Opcodes.GETFIELD, target = "Lnet/minecraft/entity/Entity;stepHeight:F", ordinal = 1))
-    )
-    private void saveOldBoundingBox(double x, double y, double z, CallbackInfo ci) {
-        arch$savedBB = this.boundingBox.copy();
-        this.boundingBox.setBB(this.boundingBox.expand(x, 0, z));
-    }
-
-    @Inject(method = "moveEntity",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/util/AxisAlignedBB;offset(DDD)Lnet/minecraft/util/AxisAlignedBB;", ordinal = 0, shift = At.Shift.BEFORE),
-            slice = @Slice(from = @At(value = "FIELD", opcode = Opcodes.GETFIELD, target = "Lnet/minecraft/entity/Entity;stepHeight:F", ordinal = 1))
-    )
-    private void restoreOldBoundingBox(double x, double y, double z, CallbackInfo ci) {
-        this.boundingBox.setBB(arch$savedBB);
-        arch$savedBB = null;
     }
 }
