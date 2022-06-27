93560a25e33ca7460e33a94c13a25647d78b00f3
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-06-27 16:43:15 -0400

    Begin migrating away from Grimoire

---
+++ src/main/java/org/embeddedt/archaicfix/asm/ArchaicMixinPlugin.java
@@ -0,0 +1,22 @@
+package org.embeddedt.archaicfix.asm;
+
+import com.falsepattern.lib.mixin.IMixin;
+import com.falsepattern.lib.mixin.IMixinPlugin;
+import com.falsepattern.lib.mixin.ITargetedMod;
+import lombok.Getter;
+import org.apache.logging.log4j.Logger;
+
+public class ArchaicMixinPlugin implements IMixinPlugin {
+    @Getter
+    private final Logger logger = IMixinPlugin.createLogger("ArchaicFix Mixins");
+
+    @Override
+    public ITargetedMod[] getTargetedModEnumValues() {
+        return new ITargetedMod[0];
+    }
+
+    @Override
+    public IMixin[] getMixinEnumValues() {
+        return Mixin.values();
+    }
+}
---
+++ src/main/java/org/embeddedt/archaicfix/asm/Mixin.java
@@ -0,0 +1,26 @@
+package org.embeddedt.archaicfix.asm;
+
+import com.falsepattern.lib.mixin.IMixin;
+import com.falsepattern.lib.mixin.ITargetedMod;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+import static com.falsepattern.lib.mixin.IMixin.PredicateHelpers.*;
+
+@RequiredArgsConstructor
+public enum Mixin implements IMixin {
+    CC_PlayerChunkViewerManager(Side.COMMON, always(), "chickenchunks.common.MixinPlayerChunkViewerManager"),
+
+    // The modFilter argument is a predicate, so you can also use the .and(), .or(), and .negate() methods to mix and match multiple predicates.
+    ;
+
+    @Getter
+    public final Side side;
+    @Getter
+    public final Predicate<List<ITargetedMod>> filter;
+    @Getter
+    public final String mixin;
+}
