9c5d6834199853d3ba33cea80fde9507ae8b9d2c
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-06-27 18:01:06 -0400

    Use the targeted mod system

--- src/main/java/org/embeddedt/archaicfix/asm/TargetedMod.java
+++ src/main/java/org/embeddedt/archaicfix/asm/TargetedMod.java
@@ -11,6 +11,14 @@ import static com.falsepattern.lib.mixin.ITargetedMod.PredicateHelpers.startsWit
 
 @RequiredArgsConstructor
 public enum TargetedMod implements ITargetedMod {
+    CHICKENCHUNKS("ChickenChunks", false, startsWith("chickenchunks")),
+    MRTJPCORE("MrTJPCore", false, startsWith("mrtjpcore")),
+    CHUNK_PREGENERATOR("ChunkPregenerator", false, startsWith("chunk+pregen")),
+    THERMALEXPANSION("ThermalExpansion", false, startsWith("thermalexpansion")),
+    GREGTECH6("GregTech", false, startsWith("gregtech")),
+    MATTEROVERDRIVE("MatterOverdrive", false, startsWith("matteroverdrive")),
+    PROJECTE("ProjectE", false, startsWith("projecte")),
+    TC4TWEAKS("TC4Tweaks", false, startsWith("thaumcraft4tweaks")),
     FASTCRAFT("FastCraft", false, startsWith("fastcraft")),
     ;
 
