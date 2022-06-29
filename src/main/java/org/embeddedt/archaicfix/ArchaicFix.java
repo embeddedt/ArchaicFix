5bbf3c3ce338816a038a9a8115a03362897c58f6
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-06-29 11:24:41 -0400

    Use mod information provided by build script

--- src/main/java/org/embeddedt/archaicfix/ArchaicFix.java
+++ src/main/java/org/embeddedt/archaicfix/ArchaicFix.java
@@ -26,8 +26,8 @@ import java.util.stream.Collectors;
 @Mod(modid = ArchaicFix.MODID, version = ArchaicFix.VERSION)
 public class ArchaicFix
 {
-    public static final String MODID = "archaicfix";
-    public static final String VERSION = "1.0";
+    public static final String MODID = Tags.MODID;
+    public static final String VERSION = Tags.VERSION;
 
     public static List<ItemStack> initialCreativeItems = null;
 
