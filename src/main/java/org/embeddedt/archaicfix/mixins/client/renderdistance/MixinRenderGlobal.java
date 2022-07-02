73bd8c83c182d1ab25dc8eebd0473281412e26c4
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-07-01 20:07:35 -0400

    Make 32 chunk render distance work

--- src/main/java/org/embeddedt/archaicfix/mixins/client/renderdistance/MixinRenderGlobal.java
+++ src/main/java/org/embeddedt/archaicfix/mixins/client/renderdistance/MixinRenderGlobal.java
@@ -5,9 +5,10 @@ import net.minecraft.client.renderer.RenderGlobal;
 import org.embeddedt.archaicfix.ArchaicFix;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.nio.Buffer;
+import java.nio.IntBuffer;
 
 @Mixin(RenderGlobal.class)
 public class MixinRenderGlobal {
@@ -15,5 +16,17 @@ public class MixinRenderGlobal {
     private int generateDisplayLists(int p_74526_0_) {
         int chunkNum = (ArchaicFix.MAX_RENDER_DISTANCE * 2) + 2;
         return GLAllocation.generateDisplayLists(chunkNum * chunkNum * 16 * 3);
+    }
+
+    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GLAllocation;createDirectIntBuffer(I)Ljava/nio/IntBuffer;"))
+    private IntBuffer createOcclusionBuffer(int p_74526_0_) {
+        int chunkNum = (ArchaicFix.MAX_RENDER_DISTANCE * 2) + 2;
+        return GLAllocation.createDirectIntBuffer(chunkNum * chunkNum * 16);
+    }
+
+    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Ljava/nio/IntBuffer;limit(I)Ljava/nio/Buffer;"))
+    private Buffer limitOcclusionBuffer(IntBuffer instance, int i) {
+        int chunkNum = (ArchaicFix.MAX_RENDER_DISTANCE * 2) + 2;
+        return instance.limit(chunkNum * chunkNum * 16);
     }
 }
