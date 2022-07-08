3b126ef44a631dfaceb4df5952f1ce0c933aa34b
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-07-08 09:04:25 -0400

    Add a thread that reloads the sound system when the stream breaks
    This fixes audio disappearing and never coming back on Linux

--- src/main/java/org/embeddedt/archaicfix/proxy/ClientProxy.java
+++ src/main/java/org/embeddedt/archaicfix/proxy/ClientProxy.java
@@ -1,11 +1,16 @@
 package org.embeddedt.archaicfix.proxy;
 
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.event.sound.SoundLoadEvent;
+import net.minecraftforge.client.event.sound.SoundSetupEvent;
 import net.minecraftforge.common.MinecraftForge;
+import org.embeddedt.archaicfix.ArchaicLogger;
 import org.embeddedt.archaicfix.config.ArchaicConfig;
+import org.embeddedt.archaicfix.helpers.SoundDeviceThread;
 import org.embeddedt.archaicfix.occlusion.OcclusionHelpers;
 import zone.rong.loliasm.api.LoliStringPool;
 
@@ -14,6 +19,8 @@ import java.util.ArrayList;
 import static org.embeddedt.archaicfix.ArchaicFix.initialCreativeItems;
 
 public class ClientProxy extends CommonProxy {
+    SoundDeviceThread soundThread = null;
+    public static volatile boolean soundSystemReloadLock = false;
     @Override
     public void preinit() {
         super.preinit();
@@ -21,6 +28,17 @@ public class ClientProxy extends CommonProxy {
         if(ArchaicConfig.enableOcclusionTweaks)
             OcclusionHelpers.init();
         MinecraftForge.EVENT_BUS.register(new LoliStringPool.EventHandler());
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onSoundSetup(SoundLoadEvent event) {
+        soundSystemReloadLock = false;
+        if(soundThread == null) {
+            ArchaicLogger.LOGGER.info("Starting sound device thread");
+            soundThread = new SoundDeviceThread();
+            soundThread.start();
+        }
     }
 
     private void fillCreativeItems() {
