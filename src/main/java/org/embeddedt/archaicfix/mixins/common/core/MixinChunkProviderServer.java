d791453cd9f3ebe4d7dcad4be124be25f607c323
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-07-11 17:18:46 -0400

    Make sure chunks populate their biomes immediately
    Fixes #6

--- src/main/java/org/embeddedt/archaicfix/mixins/common/core/MixinChunkProviderServer.java
+++ src/main/java/org/embeddedt/archaicfix/mixins/common/core/MixinChunkProviderServer.java
@@ -1,5 +1,8 @@
 package org.embeddedt.archaicfix.mixins.common.core;
 
+import net.minecraft.world.biome.WorldChunkManager;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.ChunkProviderServer;
 import net.minecraftforge.common.DimensionManager;
 import org.embeddedt.archaicfix.config.ArchaicConfig;
@@ -12,5 +15,19 @@ public class MixinChunkProviderServer {
     @Redirect(method = "unloadChunksIfNotNearSpawn", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/common/DimensionManager;shouldLoadSpawn(I)Z", remap = false))
     private boolean neverLoadSpawn(int dim) {
         return !ArchaicConfig.disableSpawnChunks && DimensionManager.shouldLoadSpawn(dim);
+    }
+
+    @Redirect(method = "originalLoadChunk", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/IChunkProvider;provideChunk(II)Lnet/minecraft/world/chunk/Chunk;"), remap = false)
+    private Chunk populateChunkWithBiomes(IChunkProvider instance, int chunkX, int chunkZ) {
+        Chunk chunk = instance.provideChunk(chunkX, chunkZ);
+        if(chunk != null) {
+            WorldChunkManager manager = chunk.worldObj.getWorldChunkManager();
+            for(int z = 0; z < 16; z++) {
+                for(int x = 0; x < 16; x++) {
+                    chunk.getBiomeGenForWorldCoords(x, z, manager);
+                }
+            }
+        }
+        return chunk;
     }
 }
