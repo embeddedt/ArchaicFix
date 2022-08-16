34a74e8465072df76cdde26c24ae7a8962943943
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-08-16 18:52:38 -0400

    Fix crash when Botania accesses client biomes via WorldChunkManager

--- src/main/java/org/embeddedt/archaicfix/mixins/common/core/MixinWorldChunkManager.java
+++ src/main/java/org/embeddedt/archaicfix/mixins/common/core/MixinWorldChunkManager.java
@@ -2,13 +2,16 @@ package org.embeddedt.archaicfix.mixins.common.core;
 
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeCache;
+import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.biome.WorldChunkManager;
 import net.minecraft.world.gen.layer.GenLayer;
+import org.embeddedt.archaicfix.ArchaicLogger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(WorldChunkManager.class)
 public class MixinWorldChunkManager {
@@ -18,6 +21,8 @@ public class MixinWorldChunkManager {
 
     @Shadow private GenLayer biomeIndexLayer;
 
+    private World clientWorld = null;
+
     @Inject(method = "<init>(Lnet/minecraft/world/World;)V", at = @At("RETURN"))
     private void noCacheOnClient(World world, CallbackInfo ci) {
         if(world.isRemote) {
@@ -25,6 +30,19 @@ public class MixinWorldChunkManager {
             this.biomeCache = null;
             this.genBiomes = null;
             this.biomeIndexLayer = null;
+            clientWorld = world;
+        }
+    }
+
+    @Inject(method = "getBiomeGenAt(II)Lnet/minecraft/world/biome/BiomeGenBase;", at = @At("HEAD"), cancellable = true)
+    private void safelyGetClientBiome(int x, int z, CallbackInfoReturnable<BiomeGenBase> cir) {
+        if(this.biomeCache == null) {
+            if(clientWorld != null)
+                cir.setReturnValue(clientWorld.getBiomeGenForCoords(x, z));
+            else {
+                ArchaicLogger.LOGGER.warn("A mod attempted to retrieve a biome client-side via a WorldChunkManager without a client world reference. This is not safe. ArchaicFix has prevented a crash but some things may not work properly.");
+                cir.setReturnValue(BiomeGenBase.ocean);
+            }
         }
     }
 }
