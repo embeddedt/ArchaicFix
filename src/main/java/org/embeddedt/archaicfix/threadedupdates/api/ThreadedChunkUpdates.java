c6e3fc62c08a8106c03186408e19097d0b6c97a6
makamys <makamys@outlook.com>
2022-09-03 03:32:23 +0200

    Add ThreadedChunkUpdates#isEnabled API method

--- src/main/java/org/embeddedt/archaicfix/threadedupdates/api/ThreadedChunkUpdates.java
+++ src/main/java/org/embeddedt/archaicfix/threadedupdates/api/ThreadedChunkUpdates.java
@@ -5,6 +5,10 @@ import org.embeddedt.archaicfix.threadedupdates.ThreadedChunkUpdateHelper;
 
 public class ThreadedChunkUpdates {
 
+    public static boolean isEnabled() {
+        return ThreadedChunkUpdateHelper.instance != null;
+    }
+
     /** Returns the thread-local tessellator instance. Can only be called after init phase. */
     public static Tessellator getThreadTessellator() {
         return ThreadedChunkUpdateHelper.instance.getThreadTessellator();
