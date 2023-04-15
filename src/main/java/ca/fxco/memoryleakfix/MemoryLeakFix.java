4919ef63507396190099815474a46099da02acf7
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-04-15 12:51:59 -0400

    Don't apply mixin forceload if Mixin version doesn't have memory leak

--- src/main/java/ca/fxco/memoryleakfix/MemoryLeakFix.java
+++ src/main/java/ca/fxco/memoryleakfix/MemoryLeakFix.java
@@ -1,26 +1,30 @@
 package ca.fxco.memoryleakfix;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.config.LoggerConfig;
 import org.embeddedt.archaicfix.ArchaicLogger;
 import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.injection.struct.InjectorGroupInfo;
 import org.spongepowered.asm.mixin.transformer.ClassInfo;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.List;
 import java.util.Map;
 
 public class MemoryLeakFix {
     public static void forceLoadAllMixinsAndClearSpongePoweredCache() {
         ArchaicLogger.LOGGER.info("Forceloading mixins and clearing mixin cache");
-        MixinEnvironment.getCurrentEnvironment().audit();
         try {
-            Field noGroupField = InjectorGroupInfo.Map.class.getDeclaredField("NO_GROUP");
+            Field noGroupField;
+            try {
+                noGroupField = InjectorGroupInfo.Map.class.getDeclaredField("NO_GROUP");
+            } catch(NoSuchFieldException e) {
+                noGroupField = null;
+            }
+            if(noGroupField == null || !Modifier.isStatic(noGroupField.getModifiers())) {
+                ArchaicLogger.LOGGER.info("No need to forceload, this mixin version doesn't have the memory leak");
+                return;
+            }
+            MixinEnvironment.getCurrentEnvironment().audit();
             noGroupField.setAccessible(true);
             Object noGroup = noGroupField.get(null);
             Field membersField = noGroup.getClass().getDeclaredField("members");
