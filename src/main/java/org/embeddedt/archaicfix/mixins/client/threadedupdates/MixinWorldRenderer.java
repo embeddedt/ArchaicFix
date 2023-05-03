bf4b240004a56c41a79e780a5a6b9de40498e603
makamys <makamys@outlook.com>
2023-05-03 14:30:58 +0200

    Fix transparent chunk layer getting added twice while moving
    Fixes #17

--- src/main/java/org/embeddedt/archaicfix/mixins/client/threadedupdates/MixinWorldRenderer.java
+++ src/main/java/org/embeddedt/archaicfix/mixins/client/threadedupdates/MixinWorldRenderer.java
@@ -1,8 +1,11 @@
 package org.embeddedt.archaicfix.mixins.client.threadedupdates;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.world.ChunkCache;
 import org.embeddedt.archaicfix.threadedupdates.ThreadedChunkUpdateHelper;
 import org.embeddedt.archaicfix.threadedupdates.ICapturableTessellator;
 import org.embeddedt.archaicfix.threadedupdates.IRendererUpdateResultHolder;
@@ -10,6 +13,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.HashSet;
 
 @Mixin(WorldRenderer.class)
 public class MixinWorldRenderer implements IRendererUpdateResultHolder {
@@ -21,8 +27,9 @@ public class MixinWorldRenderer implements IRendererUpdateResultHolder {
         ThreadedChunkUpdateHelper.lastWorldRenderer = ((WorldRenderer)(Object)this);
     }
 
-    @Inject(method = "postRenderBlocks", at = @At("HEAD"))
-    private void loadTessellationResult(int pass, EntityLivingBase view, CallbackInfo ci) {
+    @Inject(method = "updateRenderer", at = @At(value="INVOKE", target = "Lnet/minecraft/client/renderer/WorldRenderer;postRenderBlocks(ILnet/minecraft/entity/EntityLivingBase;)V"), locals=LocalCapture.CAPTURE_FAILHARD)
+    private void loadTessellationResult(EntityLivingBase cameraEntity, CallbackInfo ci, int i, int j, int k, int l, int i1, int j1, HashSet hashset, Minecraft minecraft, EntityLivingBase entitylivingbase1, int l1, int i2, int j2, byte b0, ChunkCache chunkcache, RenderBlocks renderblocks, int k2) {
+        int pass = k2;
         if(!arch$getRendererUpdateTask().cancelled) {
             ((ICapturableTessellator) Tessellator.instance).arch$addTessellatorVertexState(arch$getRendererUpdateTask().result[pass].renderedQuads);
         }
