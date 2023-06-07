6bdc1eb2fd025ac6fcdb334dc0fbd9bc7cc1e4f7
makamys <makamys@outlook.com>
2023-06-07 16:57:19 +0200

    Don't use locks, just forbid lighting updates from wrong threads
    Reduces func_147451_t time by 30~70%
    However, world gen with Phosphor still appears to be slower than without it.

--- src/main/java/org/embeddedt/archaicfix/lighting/world/lighting/LightingEngine.java
+++ src/main/java/org/embeddedt/archaicfix/lighting/world/lighting/LightingEngine.java
@@ -132,12 +132,27 @@ public class LightingEngine implements ILightingEngine {
      */
     @Override
     public void scheduleLightUpdate(final EnumSkyBlock lightType, final int xIn, final int yIn, final int zIn) {
-        this.acquireLock();
-
-        try {
+        // Upstream used locks for synchronization, we simply don't allow access from other threads because locks are
+        // too slow.
+        Thread current = Thread.currentThread();
+        if(current == this.ownedThread) {
             this.scheduleLightUpdate(lightType, encodeWorldCoord(xIn, yIn, zIn));
-        } finally {
-            this.releaseLock();
+        } else {
+            // It is NEVER valid to call World methods from a thread other than the owning thread of the world instance.
+            // Users can safely disable this warning, however it will not resolve the issue.
+            if (ENABLE_ILLEGAL_THREAD_ACCESS_WARNINGS) {
+                    IllegalAccessException e = new IllegalAccessException(String.format("World is owned by '%s' (ID: %s)," +
+                                    " but was accessed from thread '%s' (ID: %s)",
+                            this.ownedThread.getName(), this.ownedThread.getId(), current.getName(), current.getId()));
+
+                    ArchaicLogger.LOGGER.warn(
+                            "Something (likely another mod) has attempted to modify the world's state from the wrong thread!\n" +
+                                    "This is *bad practice* and can cause severe issues in your game. Phosphor has done as best as it can to mitigate this violation," +
+                                    " but it will likely introduce lighting errors.\nIn a future release, this violation may result in a hard crash instead" +
+                                    " of the current soft warning. You should report this issue to our issue tracker with the following stacktrace information.\n(If you are" +
+                                    " aware you have misbehaving mods and cannot resolve this issue, you can safely disable this warning by setting" +
+                                    " `enable_illegal_thread_access_warnings` to `false` in Phosphor's configuration file for the time being.)", e);
+            }
         }
     }
 
@@ -184,49 +199,12 @@ public class LightingEngine implements ILightingEngine {
             return;
         }
 
-        this.acquireLock();
-
-        try {
-            this.processLightUpdatesForTypeInner(lightType, queue);
-        } finally {
-            this.releaseLock();
-        }
+        this.processLightUpdatesForTypeInner(lightType, queue);
     }
 
     @SideOnly(Side.CLIENT)
     private boolean isCallingFromMainThread() {
         return Minecraft.getMinecraft().func_152345_ab();
-    }
-
-    private void acquireLock() {
-        if (!this.lock.tryLock()) {
-            // If we cannot lock, something has gone wrong... Only one thread should ever acquire the lock.
-            // Validate that we're on the right thread immediately so we can gather information.
-            // It is NEVER valid to call World methods from a thread other than the owning thread of the world instance.
-            // Users can safely disable this warning, however it will not resolve the issue.
-            if (ENABLE_ILLEGAL_THREAD_ACCESS_WARNINGS) {
-                Thread current = Thread.currentThread();
-
-                if (current != this.ownedThread) {
-                    IllegalAccessException e = new IllegalAccessException(String.format("World is owned by '%s' (ID: %s)," +
-                                    " but was accessed from thread '%s' (ID: %s)",
-                            this.ownedThread.getName(), this.ownedThread.getId(), current.getName(), current.getId()));
-
-                    ArchaicLogger.LOGGER.warn(
-                            "Something (likely another mod) has attempted to modify the world's state from the wrong thread!\n" +
-                                    "This is *bad practice* and can cause severe issues in your game. Phosphor has done as best as it can to mitigate this violation," +
-                                    " but it may negatively impact performance or introduce stalls.\nIn a future release, this violation may result in a hard crash instead" +
-                                    " of the current soft warning. You should report this issue to our issue tracker with the following stacktrace information.\n(If you are" +
-                                    " aware you have misbehaving mods and cannot resolve this issue, you can safely disable this warning by setting" +
-                                    " `enable_illegal_thread_access_warnings` to `false` in Phosphor's configuration file for the time being.)", e);
-
-                }
-
-            }
-
-            // Wait for the lock to be released. This will likely introduce unwanted stalls, but will mitigate the issue.
-            this.lock.lock();
-        }
     }
 
     private void releaseLock() {
