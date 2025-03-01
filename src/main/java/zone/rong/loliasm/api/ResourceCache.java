a8bf1afff29115c0bf8f621f147e3355ffd25c7f
glowredman <35727266+glowredman@users.noreply.github.com>
2025-03-01 13:03:29 +0100

    Add serialVersionUIDs

--- src/main/java/zone/rong/loliasm/api/ResourceCache.java
+++ src/main/java/zone/rong/loliasm/api/ResourceCache.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 
 public class ResourceCache extends HashMap<String, byte[]> {
 
+    private static final long serialVersionUID = 3922779088970477966L;
+
     public byte[] add(String s, byte[] bytes) {
         return super.put(s, bytes);
     }
