d283a17d3095021d0946e2079305886f0afc56d8
glowredman <35727266+glowredman@users.noreply.github.com>
2025-03-03 18:57:31 +0100

    Fix injection point

--- src/main/java/org/embeddedt/archaicfix/mixins/common/extrautils/MixinTileEntityTrashCan.java
+++ src/main/java/org/embeddedt/archaicfix/mixins/common/extrautils/MixinTileEntityTrashCan.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinTileEntityTrashCan {
     @Shadow(remap = false) private boolean added;
 
-    @Inject(method = "readFromNBT", at = @At("TAIL"), remap = false)
+    @Inject(method = "readFromNBT", at = @At("TAIL"))
     private void forceRescan(NBTTagCompound par1NBTTagCompound, CallbackInfo ci) {
         this.added = true;
     }
